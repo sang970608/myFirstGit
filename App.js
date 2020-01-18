53c08dea11b83aed80b010f4af833bf83fdbec58
sang970608 <sang970608@gmail.com>
2020-01-18 19:01:22 +0900

    20200118 재수정 15/17분량

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Loading from './Loading'; //Loading.js 불러옴
-import * as Location from 'expo-location'; //위치 native
+import * as Location from "expo-location"; //위치 native
 import {Alert} from "react-native"; //경고 native
 import axios from "axios";
 import Weather from './Weather';
@@ -8,37 +8,45 @@ import Weather from './Weather';
 const API_KEY = "e6103d0d1dc2e3c43e72b79fa3f95302";
 
 export default class App extends React.Component {
-  state = {
-    isLoading : true 
+  state={
+    isLoading : true
   };
 
-  getWeather = async(latitude, longitude)=>{
-    const { data } = await axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+  getWeather = async(latitude,longitude) =>{
+    const { 
+      data: {
+        main :{temp},
+        weather
+      }
+     } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
       );
-      this.setState({ isLoading:false, temp: data.main.temp} );
-    }
+      this.setState({
+        isLoading:false,
+        condition:weather[0].main,
+        temp 
+      });
+      //temp의 값을 Object에서 가져옴
+  };
 
-  getLocation = async()=>{ //비동기화 함수
+  getLocation = async() => {
     try{
-      await Location.requestPermissionsAsync() //위치 정보를 받으면 넘어가도록 await
-      const {coords : {latitude,longitude}} = await Location.getCurrentPositionAsync();
-      //위치 정보값에서 latitude,longitude값을 가져옴
-      this.getWeather(latitude, longitude)
-      this.setState({ isLoading: false}); //Loading을 false로
-    }
-    catch (error){
-      Alert.alert("Can't find you", "So sad");
+      await Location.requestPermissionsAsync();
+      const { coords: {latitude, longitude} 
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude,longitude);
+      this.setState({ isLoading: false });
+    }catch(error){
+      Alert.alert("Can`t find you.","So sad");
     }
   }
-  
+
   componentDidMount(){
     this.getLocation();
   }
 
   render(){
-    const { isLoading, temp } = this.state; //this.state.isLoading을 생략하기 위함
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)}  />;
-    //is Loading이면 그대로 출력, 아니면 null값 출력
+    const { isLoading, temp , condition} = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition}/>;
   }
 }
--- Weather.js
+++ Weather.js
@@ -1,20 +1,50 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { StyleSheet, View, Text } from "react-native";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
 
-export default function Weather(){
+export default function Weather({temp}){
     return <View style={styles.container}>
-    <Text></Text></View>
-}
+    <LinearGradient
+    colors={['#4c669f', '#3b5998', '#192f6a']}
+    style={{ padding: 15, alignItems: 'center', borderRadius: 5 }}>
+        <View style={styles.halfContainer}>
+        <MaterialCommunityIcons size={96} name="weather-lightning-rainy"></MaterialCommunityIcons>
+        <Text style={styles.temp}>{temp}°C</Text>
+        </View>
+        <View style={styles.halfContainer} />
+        </LinearGradient>
+    </View>
+};
 
 Weather.propTypes = {
-    temp:PropTypes.number.isRequired
+    temp:PropTypes.number.isRequired,
+    condition: PropTypes.oneOf([
+        "Thunderstorm",
+        "Drizzle",
+        "Rain",
+        "Snow",
+        "Atmosphere",
+        "Clear",
+        "Clouds",
+        "Haze",
+        "Mist",
+        "Dust"
+    ]).isRequired
 };
 
 const styles = StyleSheet.create({
     container : {
         flex : 1,
         justifyContent : "center",
+        alignItems: "center"
+    },
+    temp:{
+        fontSize:42
+    },
+    halfContainer: {
+        flex: 1,
+        justifyContent: "center",
         alignItems: "center"
     }
 })
